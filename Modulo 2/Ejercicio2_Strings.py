35b49e1c10873b4899fafd4fd22b25a9bb3d84f2
Sonii2001 <salvaradore8307@uaemex.mx>
2026-03-10 12:33:17 -0600

    Add files via upload
    Subida de ejercicios realizados en clase, asi como del proyecto

---
+++ Modulo 2/Ejercicio2_Strings.py
@@ -0,0 +1,16 @@
+parrafo1 = "Cada día te quiero más y nunca lo he olvidado." 
+parrafo2 = "En el corazón de una historia de amor tecnológica, Lucas y Carla compartían su pasión por la programación. Cada día, mientras aprendían a programar, sus corazones se sincronizaban con cada línea de código." 
+parrafo3 = "En cada amanecer, veo el brillo de tus ojos; en cada atardecer, siento el calor de tu abrazo; y en cada noche estrellada, me pierdo en el infinito de tu amor."
+
+print("Mensajes Secretos Decodificados")
+print("Mensaje secreto 1")
+print(parrafo1 [1::18])
+print("Mensaje secreto 2")
+print(parrafo2 [138:147])
+print("Mensaje secreto 3")
+print(parrafo3 [125])
+print(parrafo3 [94])
+print(parrafo3 [35])
+print(parrafo3 [107])
+print(parrafo3 [20])
+print(parrafo3 [1])
