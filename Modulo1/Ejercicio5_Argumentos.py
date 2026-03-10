cae4a67044764ad8ab517d904029892af33b34d1
Sonii2001 <salvaradore8307@uaemex.mx>
2026-03-10 12:24:47 -0600

    Add files via upload
    Subida de ejercicios realizados en clase, así como del proyecto

---
+++ Modulo1/Ejercicio5_Argumentos.py
@@ -0,0 +1,13 @@
+def Organizar_Fiesta (invitados, tema="Python", lugar="Aula de Informatica"):
+    
+    print(f"Preparando una fiesta para {invitados} invitados")
+    print(f"Tema de la fiesta: {tema}")
+    print(f"Lugar de la fiesta: {lugar}")
+
+Organizar_Fiesta(25)
+print()
+Organizar_Fiesta(50, tema="Intercambio de equipos", lugar = "UAEMEX")
+print()
+Organizar_Fiesta(100, tema="Disfraces", lugar = "Casa")
+
+    
